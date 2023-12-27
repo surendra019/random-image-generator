0069d89e2c8fb7a1cf9150759132b44765d69d5e
surendra019 <iamsurendra019@gmail.com>
2023-12-27 21:05:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const pathToFile = path.join(__dirname, 'body.html')
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res)=>{
+res.sendFile(pathToFile);
+})
+
+app.post(("/search"), (req, res)=>{
+    res.end();
+    // res.responseCode(204).end();
+});
+app.listen(4000);
